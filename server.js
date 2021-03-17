0de3f4032540f29faccc39337ea4f67196d3b6e6
dkristof99 <kd648@live.mdx.ac.uk>
2021-03-17 17:01:38 +0000

    230

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://dkristof:1234@cluster0.wkaio.mongodb.net/CW3?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("CW3").collection("products");
   // perform actions on the collection object
   client.close();
 });
