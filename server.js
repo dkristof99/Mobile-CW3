e8e1aa2ab7168f4d207976b2af4c322f59af61d3
dkristof99 <kd648@live.mdx.ac.uk>
2021-03-17 15:36:49 +0000

    56156

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 
 const MongoClient = require("mongodb").MongoClient;
 let db;
-MongoClient.connect("mongodb+srv://dkristof:1234@cluster0.wkaio.mongodb.net/webstore?retryWrites=true&w=majority", (err, client) => {
+MongoClient.connect("mongodb+srv://dkristof:1234@cluster0.wkaio.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", (err, client) => {
     db = client.db("CW3")
 })
 
